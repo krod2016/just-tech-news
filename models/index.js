7f2e3e522787fdb73c76aa889be7a69026466764
krod2016 <katrinajrodriguez1993@gmail.com>
2022-02-18 21:54:54 -0600

    updated comment routes

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 // import all models
 const Post = require('./Post');
 const User = require('./user');
-const Vote = require('./vote');
+const Vote = require('./Vote');
+const Comment = require('./comments');
 
 // create associations
 User.hasMany(Post, {
@@ -40,4 +41,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
